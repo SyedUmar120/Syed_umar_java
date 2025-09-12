3200ecb33f45c1588d481f7a76f731cc6f2baf57
SyedUmar120 <syedumarscse2427@ksrce.ac.in>
2025-09-12 18:26:22 +0530

    Add files via upload

---
+++ reverse  string.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        int n =123;
+        int rev=0;
+        int digit=0;
+        while(n!=0){
+            digit=n%10;
+            rev=rev*10+digit;
+            n/=10;
+        }
+        System.out.print(rev);
+        }
+}
