9e512641c5d6758adc335a300160d3b1647388ee
SyedUmar120 <syedumarscse2427@ksrce.ac.in>
2025-09-15 15:58:49 +0530

    Add files via upload

---
+++ add 2 num using array.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int[][] a=new int[n][n];
+        int[][] b=new int[n][n];
+        int[][] c=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+        a[i][j]=sc.nextInt();
+        b[i][j]=sc.nextInt();
+    }
+  }
+  for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+          c[i][j]=a[i][j]+b[i][j];
+      }
+  }
+  for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+          System.out.println(c[i][j]+" ");
+       }
+        System.out.println();
+     }
+    sc.close();
+   }
+}
