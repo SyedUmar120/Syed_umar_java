e1e7a1fd2d5cf42caaf2494a2f5529dacfc696a3
SyedUmar120 <syedumarscse2427@ksrce.ac.in>
2025-09-19 18:51:55 +0530

    Add files via upload

---
+++ error handling.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+    try{    int a=10,b=10;
+        System.out.println("divisible");
+    }
+    catch(Exception e){
+        System.out.println("can't divide");
+        
+    }
+    finally{
+        System.out.println("something");
+    }
+    }
+}
